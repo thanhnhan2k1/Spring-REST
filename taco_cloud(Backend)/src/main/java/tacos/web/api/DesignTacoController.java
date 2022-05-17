61fcbf883f290630a6f1bd9e1fd1590a719affff
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2022-05-17 12:17:09 +0700

    Add files via upload

---
+++ taco_cloud(Backend)/src/main/java/tacos/web/api/DesignTacoController.java
@@ -0,0 +1,92 @@
+package tacos.web.api;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.Taco;
+import tacos.data.TacoRepository;
+import tacos.Ingredient;
+import tacos.Ingredient.Type;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class DesignTacoController {
+	private RestTemplate rest = new RestTemplate();
+	private TacoRepository tacoRepo;
+	@Autowired
+	//EntityLinks entityLinks;
+	public DesignTacoController(TacoRepository tacoRepo) {
+	this.tacoRepo = tacoRepo;
+	}
+	@GetMapping("/recent")
+	public Iterable<Taco> recentTacos() {
+	return tacoRepo.findAll();
+	}
+	@ModelAttribute
+	public void addIngredientsToModel(Model model) {
+		List<Ingredient> ingredients =
+				Arrays.asList(rest.getForObject("http://localhost:8080/ingredients",Ingredient[].class));
+		  Type[] types = Ingredient.Type.values();
+			for (Type type : types) {
+				model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+			}
+		}
+
+		@GetMapping
+		public String showDesignForm(Model model) {
+			model.addAttribute("taco", new Taco());
+			return "design";
+		}
+
+		private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+			List<Ingredient> ingrList = new ArrayList<Ingredient>();
+			for (Ingredient ingredient: ingredients) {
+				if (ingredient.getType().equals(type)) ingrList.add(ingredient);
+			}
+			return ingrList;
+				
+		}
+		
+		@PostMapping(consumes = "application/json")
+		@ResponseStatus(HttpStatus.CREATED)
+		public Taco postTaco(@RequestBody Taco taco) {
+		return tacoRepo.save(taco);
+		}
+		@PostMapping
+		public String processDesign(@RequestParam("ingredients") String
+		ingredientIds, @RequestParam("name") String name) {
+		List<Ingredient> ingredients = new ArrayList<Ingredient>();
+		for (String ingredientId : ingredientIds.split(",")) {
+		Ingredient ingredient = rest.getForObject("http://localhost:8080/ingredients/{id}",Ingredient.class, ingredientId);
+		ingredients.add(ingredient);
+		}
+		Taco taco = new Taco();
+		taco.setName(name);
+		taco.setIngredients(ingredients);
+		System.out.println(taco);
+		rest.postForObject("http://localhost:8080/design", taco, Taco.class);
+		return "redirect:/orders/current";
+		}
+}
---
+++ taco_cloud/src/main/java/tacos/Ingredient.java
@@ -0,0 +1,27 @@
+package tacos;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.AccessLevel;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor(force=true)
+@Entity
+
+public class Ingredient {
+	  private final String id;
+	  private final String name;
+	  private final Type type;
+	  public static enum Type {
+	    WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+	  } 
+
+}
+
+
