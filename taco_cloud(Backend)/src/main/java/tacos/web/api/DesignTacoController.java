73681eb095afef4ba003b758999d0ed767d2f6da
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2022-05-23 22:05:34 +0700

    Update DesignTacoController.java

--- taco_cloud(Backend)/src/main/java/tacos/web/api/DesignTacoController.java
+++ taco_cloud(Backend)/src/main/java/tacos/web/api/DesignTacoController.java
@@ -3,6 +3,7 @@ package tacos.web.api;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 
@@ -14,6 +15,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,60 +35,32 @@ import tacos.Ingredient.Type;
 @RequestMapping(path = "/design", produces = "application/json")
 @CrossOrigin(origins = "*")
 public class DesignTacoController {
-	private RestTemplate rest = new RestTemplate();
+
 	private TacoRepository tacoRepo;
+
 	@Autowired
-	//EntityLinks entityLinks;
+	// EntityLinks entityLinks;
 	public DesignTacoController(TacoRepository tacoRepo) {
-	this.tacoRepo = tacoRepo;
+		this.tacoRepo = tacoRepo;
 	}
+
 	@GetMapping("/recent")
 	public Iterable<Taco> recentTacos() {
-	return tacoRepo.findAll();
+		return tacoRepo.findAll();
 	}
-	@ModelAttribute
-	public void addIngredientsToModel(Model model) {
-		List<Ingredient> ingredients =
-				Arrays.asList(rest.getForObject("http://localhost:8080/ingredients",Ingredient[].class));
-		  Type[] types = Ingredient.Type.values();
-			for (Type type : types) {
-				model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
-			}
-		}
 
-		@GetMapping
-		public String showDesignForm(Model model) {
-			model.addAttribute("taco", new Taco());
-			return "design";
+	@GetMapping("/{id}")
+	public Taco tacoById(@PathVariable("id") Long id) {
+		Optional<Taco> optTaco = tacoRepo.findById(id);
+		if (optTaco.isPresent()) {
+			return optTaco.get();
 		}
+		return null;
+	}
 
-		private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-			List<Ingredient> ingrList = new ArrayList<Ingredient>();
-			for (Ingredient ingredient: ingredients) {
-				if (ingredient.getType().equals(type)) ingrList.add(ingredient);
-			}
-			return ingrList;
-				
-		}
-		
-		@PostMapping(consumes = "application/json")
-		@ResponseStatus(HttpStatus.CREATED)
-		public Taco postTaco(@RequestBody Taco taco) {
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Taco postTaco(@RequestBody Taco taco) {
 		return tacoRepo.save(taco);
-		}
-		@PostMapping
-		public String processDesign(@RequestParam("ingredients") String
-		ingredientIds, @RequestParam("name") String name) {
-		List<Ingredient> ingredients = new ArrayList<Ingredient>();
-		for (String ingredientId : ingredientIds.split(",")) {
-		Ingredient ingredient = rest.getForObject("http://localhost:8080/ingredients/{id}",Ingredient.class, ingredientId);
-		ingredients.add(ingredient);
-		}
-		Taco taco = new Taco();
-		taco.setName(name);
-		taco.setIngredients(ingredients);
-		System.out.println(taco);
-		rest.postForObject("http://localhost:8080/design", taco, Taco.class);
-		return "redirect:/orders/current";
-		}
+	}
 }
